292c0e85f92915193e257050e8774c06a53d32fd
VirgaP <virginija.prasmickaite@gmail.com>
2018-12-06 14:12:48 +0200

    first commit

---
+++ src/main/resources/public/precache-manifest.a8e6964eb977e3b4659b584f2b3cedd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "77a19e4e56db9cdda8f1",
+    "url": "./static/js/main.77a19e4e.chunk.js"
+  },
+  {
+    "revision": "023bbb8f9be2e1fd1ba1",
+    "url": "./static/js/1.023bbb8f.chunk.js"
+  },
+  {
+    "revision": "77a19e4e56db9cdda8f1",
+    "url": "./static/css/main.2c13ccdf.chunk.css"
+  },
+  {
+    "revision": "023bbb8f9be2e1fd1ba1",
+    "url": "./static/css/1.1f6897be.chunk.css"
+  },
+  {
+    "revision": "82c1024397a67fbdb6a33e7b5cab9c53",
+    "url": "./index.html"
+  }
+];
